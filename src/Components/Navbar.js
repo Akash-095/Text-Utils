ccd7e5ec7fe4cdc9fb81a2ae9740ea1baee26ee9
Akash-095 <akashkumarverma369@gmail.com>
2023-07-10 21:10:34 +0530

    deploying

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 
 export default function Navbar(props)
 {
+  let Location = useLocation();
     return(
         <nav className={`navbar navbar-expand-lg navbar-dark bg-${props.mode==='dark'?'dark':'primary'}`}>
   <div className="container-fluid">
@@ -14,10 +15,10 @@ export default function Navbar(props)
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/">Home</Link>
+          <Link className={`nav-link ${Location.pathname==="/"?"active":""}`} aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/About">About</Link>
+          <Link className={`nav-link ${Location.pathname==="/About"?"active":""}`} to="/About">About</Link>
         </li>
        
           
